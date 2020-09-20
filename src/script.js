3e5b02f50c23aba3376e219d3fd7a35c1d5b1e14
dianafountaine <67707763+dianafountaine@users.noreply.github.com>
2020-09-20 00:42:23 -0800

    changed src link

--- src/script.js
+++ src/script.js
@@ -21,24 +21,18 @@ function FormatDate(date) {
     return `${day} ${hours}:${minutes}`;
   }
   
-  function displayTemperature(response) {
-    let temperatureElement = document.querySelector("#temperature");
-    let cityElement = document.querySelector("#city");
-    let humidityElement = document.querySelector("#humidity");
-    let windElement = document.querySelector("#wind");
-    let iconElement = document.querySelector("#icon");
-
-  
-    temperatureElement.innerHTML = Math.round(imperialTemperature);
-    cityElement.innerHTML = response.data.name;
-    descriptionElement.innerHTML = response.data.weather[0].description;
-    humidityElement.innerHTML = response.data.main.humidity;
-    windElement.innerHTML = Math.round(response.data.wind.speed);
-    iconElement.setAttribute(
-      "src",
-      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  function displayWeatherCondition(response) {
+    document.querySelector("#city").innerHTML = response.data.name;
+    document.querySelector("#temperature").innerHTML = Math.round(
+      response.data.main.temp
     );
-    iconElement.setAttribute("alt", response.data.weather[0].description);
+    document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+    document.querySelector("#condition").innerHTML =
+      response.data.weather[0].description;
+    document.querySelector("#wind").innerHTML = Math.round(
+      response.data.wind.speed
+    );
+    document.querySelector("src").innerHTML = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   }
   
   
